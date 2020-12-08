ba9737cd75c309fd127c0e4a54082da96b55aa1e
deandraharvin <deandraharvin3@gmail.com>
2020-12-07 19:18:13 -0500

    creating own prime factorization before starting actual simulator

--- shors.py
+++ shors.py
@@ -1,13 +1,31 @@
-class Shors():
+import math
+
+class Shors:
     def __init__(self, num, divisor):
         self.num = num
         self.div = divisor
 
+    def isprime(self, number):
+        if number % 2 == 0:
+            return False
+        for i in range(3, number, 2):
+            if number % i == 0:
+                return False
+        return True
+
     def gcd(self):
-        if self.div > self.num:
-            self.num, self.div = self.div, self.num
-        while self.div > 0:
-            self.num = self.num % self.div
-            self.num, self.div = self.div, self.num
-        return self.num
+        factors = []
+        if self.isprime(self.num):
+            return self.num
+        # #a = math.random.randint(2, self.num-1)
+        # if self.div > self.num:
+        #     self.num, self.div = self.div, self.num
+        # while self.div > 0:
+        #     self.num = self.num % self.div
+        #     self.num, self.div = self.div, self.num
+        for num in range(3, self.num, 2):
+            if self.num % num == 0:
+                if self.isprime(num):
+                    factors.append(num)
+        return factors
 
